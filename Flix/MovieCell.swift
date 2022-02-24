7a218a285d099d795e1eb07c88dc74b800427928
Antonio Razo Jr <69607553+antony361@users.noreply.github.com>
2022-02-23 22:18:49 -0800

    Files Upload

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Tony Razo on 2/22/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
